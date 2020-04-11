e4cdb43f4dd4fe2122f914df19ccca89d036deab
larry armstrong <larry.armstrong15@gmail.com>
2020-04-11 19:21:06 -0400

    Create BasicDataTypes.c

---
+++ BasicDataTypes.c
@@ -0,0 +1,14 @@
+int main() {
+    int i; 
+    long l; 
+    char c; 
+    float f;  
+    double d;
+
+    scanf("%i %ld %c %f %lf",&i,&l,&c,&f,&d);
+
+    
+    printf("%i\n%ld\n%c\n%f\n%lf",i,l,c,f,d);
+
+    return 0;
+}
