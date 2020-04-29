2ba28a48826bb0c03d61a4f6b0d92d4798931bda
larry armstrong <larry.armstrong15@gmail.com>
2020-04-29 19:11:25 -0400

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -19,17 +19,17 @@ public class Solution {
     int k = 0;
     StringBuilder sb = new StringBuilder();
 
-    textL = s.replaceAll("\\s+","");
-    L = textL.length();
+    textL = s.replaceAll("\\s+",""); //remove spaces in text
+    L = textL.length(); //get the length of string text
 
-    num = Math.sqrt(L);
+    num = Math.sqrt(L); // compute the square root
 
-    column = (int) Math.ceil(num);
-    row = (int) Math.floor(num);
+    column = (int) Math.ceil(num); //get the higher end of square root number
+    row = (int) Math.floor(num); //get the lower end of square root number
 
-    total = row * column;
+    total = row * column; //calculate nxn matrix
 
-    if(total < L){
+    if(total < L){ //if the nxn matrix is lower than total length we want to go with smallest possible nxn matrix
     row = column;
     if((column*row) < L){
     row = (int) Math.floor(num);
@@ -44,17 +44,17 @@ public class Solution {
     column = column;
     }
 
-    arr = new String[row][column];
-    textarr = textL.split("");
+    arr = new String[row][column]; //create our final nxn matrix
+    textarr = textL.split("");//split the text character by character
 
     for(int i = 0; i < row; i++){
     for(int j = 0; j < column; j++){
     if(k < textL.length()){
-    arr[i][j] = textarr[k];
+    arr[i][j] = textarr[k]; //store text in nxn matrix
     k++;
     }
     else{
-    arr[i][j] = " ";
+    arr[i][j] = " "; //if no more characters, fill array with spaces
     k++;
     }
     }
@@ -62,13 +62,13 @@ public class Solution {
 
     for(int i1 = 0; i1 < column; i1++){
         for(int j1 = 0; j1 < row; j1++){
-            sb.append(arr[j1][i1].trim());
+            sb.append(arr[j1][i1].trim()); //begin encryption by extract text row by row, also trim unecessary spacing
         }
         sb.append(" ");
     }
-    result = sb.toString().trim();
+    result = sb.toString().trim(); //store string text in result using string builder, trim trailing spaces
 
-    return result;
+    return result; //return result
 
     }
 
